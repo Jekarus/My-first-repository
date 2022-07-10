9f8f7179fd7976279996b9d89026eeb48701299e
JekaRus <83099517+Jekarus@users.noreply.github.com>
2022-07-10 20:09:56 +0300

    Update add_text_file.py

--- add_text_file.py
+++ add_text_file.py
@@ -8,12 +8,12 @@ def add_text(user):
     r = cur.execute('SELECT * FROM akk').fetchall()     # считываем все данные с файла sqlite
     my_file = open("my_akk_data.txt", "w+")
     total = 0
-    for row in r:   # отображение данных в меню
+    for row in r:   # считываем и записываем в текстовый файл
         my_file.write("Имя аккаунта: " + row[0] + "\n")
         my_file.write("Логин: " + row[1] + "\n")
         my_file.write("Пароль: " + row[2] + "\n")
         my_file.write("_______________\n")
-        total += 1
+        total += 1  # считаем общее количество аккаунтов
     my_file.write("Всего аккаунтов: " + str(total))
-    my_file.close()
+    my_file.close()     # закрываем и тем же сохраняем текстовый файл
     print("Данные успешно выведены!")
